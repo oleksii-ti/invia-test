2a4f4fad5332d8480a86a9b9e8fc0b3d9875aace
oleksii <oleksii.ti@gmail.com>
2024-11-20 17:55:41 +0100

    close #3 Create tests for basic functionalities check on the Homepage

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -3,10 +3,41 @@ import type { Locator, Page } from '@playwright/test'
 export class HomePage {
   constructor(readonly page: Page) {}
 
+  // My Account 
   myAccountIcon(): Locator {
-    return this.page.locator(`[data-test-id="shared-header-headercontrol-burgermenu"] a`)
+    return this.page.locator('[data-testid="shared-header-headercontrol-login"]')
   }
+
+  loginButton(): Locator {
+    return this.page.locator('[data-testid="shared-header-headercontrol-login"] a')
+  }
+
+  // Search Form
   departureAirport(): Locator {
-    return this.page.locator(`[data-testid="airports-field"]`)
+    return this.page.locator('[data-testid="airports-field"]')
+  }
+
+  datesField(): Locator {
+    return this.page.locator('[data-testid="travel-period-field"]')
+  }
+  destinationFields(): Locator {
+    return this.page.locator('[data-testid="destination-field"]')
+  }
+  travellersField(): Locator {
+    return this.page.locator('[data-testid="travellers-field-multiroom"]')
+  }
+  searchButton(): Locator {
+    return this.page.locator('[data-testid="submit"]')
+  }
+
+  // Sections
+  topDestinationsSection(): Locator {
+    return this.page.locator('[data-cy="home-popularDestinations"]')
+  }
+  currentOffersSection(): Locator {
+    return this.page.locator('[data-cy="search-seomulti-main-currentOffers"]')
+  }
+  articlesSection(): Locator {
+    return this.page.locator('[data-cy="search-seomulti-main-magazineArticles"]')
   }
 }
--- tests/home-page.test.ts
+++ tests/home-page.test.ts
@@ -1,11 +1,34 @@
 import { expect, test } from '../src/fixtures/fixtures'
 
+
+// Basic functionalities check on the Homepage:
+// • Navigate to the homepage for each site.
+// • Verify the presence of the following elements: o Destination, date,
+//   transportation, and passenger selectors
+// • Login button
+// • Top destinations, Current offers, Articles sections
 test.describe('Home Page', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
   })
 
-  test(`should contain search form`, async ({ homePage }) => {
+  test('should contain tours search form', async ({ homePage }) => {
     expect(homePage.departureAirport()).toBeVisible
+    expect(homePage.datesField()).toBeVisible
+    expect(homePage.destinationFields()).toBeVisible
+    expect(homePage.travellersField()).toBeVisible
+    expect(homePage.searchButton()).toBeVisible
+  })
+
+  test('should contain login link in My Account menu', async ({ homePage }) => {
+    expect(homePage.myAccountIcon()).toBeVisible
+    await homePage.myAccountIcon().hover()
+    expect(homePage.datesField()).toBeVisible
+  })
+
+  test('should contain Top destinations, Current offers, Articles sections', async ({ homePage }) => {
+    expect(homePage.topDestinationsSection()).toBeVisible
+    expect(homePage.currentOffersSection()).toBeVisible
+    expect(homePage.articlesSection()).toBeVisible
   })
 })
