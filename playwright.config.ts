566cc1b774458c746d807f9acf390f24eddc04dc
oleksii <oleksii.ti@gmail.com>
2024-11-25 05:42:36 +0100

    rename playwright project

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,10 +7,7 @@ export default defineConfig({
   timeout: 1 * 60000,
   fullyParallel: true,
   workers: 1,
-  reporter: [
-    ['list'], 
-    ['html', { open: 'never', outputFolder: `playwright-report-${process.env.LOCALE}` }]
-  ],
+  reporter: [['list'], ['html', { open: 'never', outputFolder: `playwright-report-${process.env.LOCALE}` }]],
   use: {
     baseURL: process.env.BASE_URL,
     screenshot: 'only-on-failure',
@@ -18,13 +15,15 @@ export default defineConfig({
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36',
     locale: 'en-US',
     viewport: { width: 1920, height: 1080 },
-    trace: 'retain-on-first-failure'
+    trace: 'retain-on-first-failure',
   },
 
   projects: [
     {
-      name: 'e2e',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'],
+        isMobile: false
+       },
     },
   ],
 })
