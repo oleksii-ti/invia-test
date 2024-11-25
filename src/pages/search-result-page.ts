2b3b1ea97f1318254c0256930600c198bcbb81dc
oleksii <oleksii.ti@gmail.com>
2024-11-25 03:49:54 +0100

    #15 add parametrization for search test

--- src/pages/search-result-page.ts
+++ src/pages/search-result-page.ts
@@ -1,7 +1,10 @@
 import type { Locator, BrowserContext, Page } from '@playwright/test'
 
 export class SearchResultPage {
-  constructor(readonly page: Page, readonly context: BrowserContext) {}
+  constructor(
+    readonly page: Page,
+    readonly context: BrowserContext
+  ) {}
 
   async showMoreResults() {
     await Promise.all([
@@ -14,8 +17,8 @@ export class SearchResultPage {
   async goToOffer(number: number) {
     const [offerPagePromise] = await Promise.all([
       this.context.waitForEvent('page'),
-      this.page.locator('[data-testid="find-hotellist-hotelbox-cta"]').nth(number).click()
-    ]);
+      this.page.locator('[data-testid="find-hotellist-hotelbox-cta"]').nth(number).click(),
+    ])
     const offerPage = await offerPagePromise
 
     await offerPage.waitForURL('/find/hotel/**')
@@ -25,10 +28,10 @@ export class SearchResultPage {
   }
 
   async getOfferInformation(number: number): Promise<Record<string, string>> {
-    const mainSelector = '[data-testid="hotel-info"]';
-  
+    const mainSelector = '[data-testid="hotel-info"]'
+
     const getText = async (selector: string): Promise<string> => {
-      const textContent = await this.page.locator(`${mainSelector} ${selector}`).nth(number).innerText();
+      const textContent = await this.page.locator(`${mainSelector} ${selector}`).nth(number).innerText()
       return textContent?.trim() || ''
     }
     let offerInfo = {
--- tests/tour-search.test.ts
+++ tests/tour-search.test.ts
@@ -1,65 +1,66 @@
 import { expect, test } from '../src/fixtures/fixtures'
 import { OfferPage } from '../src/pages/offer-page'
+const countries = ['Egypt']
 
 // Test Search result page
 test.describe('Tour Search', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
   })
+  for (const country of countries) {
+    // Input a test destination (e.g., "Egypt") in the search bar.
+    // Select a travel date range using the date picker.
+    // Submit the search form and wait for the results page to load.
+    test(`should open search results page for ${country} country`, async ({ homePage, page }) => {
+      await homePage.destinationFields().click()
+      await homePage.destinationFields().fill(country)
+      await homePage.autoconmpleteItem(0).click()
+      await homePage.datesField().click()
+      await homePage.setDate('2025-07-02', '2025-07-12')
+      await homePage.clickSearchButton()
+      expect(page.url()).toContain('/find/hotels')
+    })
 
-  // Input a test destination (e.g., "Egypt") in the search bar.
-  // Select a travel date range using the date picker.
-  // Submit the search form and wait for the results page to load.
-  test('should open search results page', async ({ homePage, page }) => {
-    await homePage.destinationFields().click()
-    await homePage.destinationFields().fill('Egy')
-    await homePage.autoconmpleteItem(0).click()
-    await homePage.datesField().click()
-    await homePage.setDate('2025-07-02', '2025-07-12')
-    await homePage.clickSearchButton()
-    expect(page.url()).toContain('/find/hotels')
-  })
+    // Navigate to the second page of search results.
+    // Select any offer.
+    // Verify the text on offer page correspond to offer informaion from search list
+    test(`should search for ${country} country and open second search page and verify offer information`, async ({ homePage, searchResultPage }) => {
+      await homePage.destinationFields().click()
+      await homePage.destinationFields().fill(country)
+      await homePage.autoconmpleteItem(0).click()
+      await homePage.datesField().click()
+      await homePage.setDate('2025-07-02', '2025-07-12')
+      await homePage.clickSearchButton()
+      const offerInfo = await searchResultPage.getOfferInformation(2)
+      const offerTab = await searchResultPage.goToOffer(2)
+      const offerPage = new OfferPage(offerTab)
+      expect((await offerPage.hotelRegion().innerText()).trim()).toEqual(offerInfo.region)
+      expect((await offerPage.hotelName().innerText()).trim()).toEqual(offerInfo.name)
+      expect((await offerPage.hotelRating().innerText()).trim()).toEqual(offerInfo.rating)
+      expect((await offerPage.hotelRecommendation().innerText()).trim()).toEqual(offerInfo.recommendation)
+    })
 
-  // Navigate to the second page of search results.
-  // Select any offer.
-  // Verify the text on offer page correspond to offer informaion from search list
-  test('should open second search page and verify offer information', async ({ homePage, searchResultPage }) => {
-    await homePage.destinationFields().click()
-    await homePage.destinationFields().fill('Egy')
-    await homePage.autoconmpleteItem(0).click()
-    await homePage.datesField().click()
-    await homePage.setDate('2025-07-02', '2025-07-12')
-    await homePage.clickSearchButton()
-    const offerInfo = await searchResultPage.getOfferInformation(2)
-    const offerTab = await searchResultPage.goToOffer(2)
-    const offerPage = new OfferPage(offerTab)
-    expect((await offerPage.hotelRegion().innerText()).trim()).toEqual(offerInfo.region)
-    expect((await offerPage.hotelName().innerText()).trim()).toEqual(offerInfo.name)
-    expect((await offerPage.hotelRating().innerText()).trim()).toEqual(offerInfo.rating)
-    expect((await offerPage.hotelRecommendation().innerText()).trim()).toEqual(offerInfo.recommendation)
-  })
+    // Navigate to the second page of search results.
+    // Select any offer.
+    // Confirm the selection by clicking on the offer box.
+    test(`should open second search page and open any hotel info box for ${country} country`, async ({ homePage, searchResultPage }) => {
+      await homePage.destinationFields().click()
+      await homePage.destinationFields().fill('Egy')
+      await homePage.autoconmpleteItem(0).click()
+      await homePage.datesField().click()
+      await homePage.setDate('2025-07-02', '2025-07-12')
+      await homePage.clickSearchButton()
+      const offerInfo = await searchResultPage.getOfferInformation(2)
 
+      const offerTab = await searchResultPage.goToOffer(2)
+      const offerPage = new OfferPage(offerTab)
+      const counfirmedOffers = await offerPage.countConfirmedOffers()
 
-  // Navigate to the second page of search results.
-  // Select any offer.
-  // Confirm the selection by clicking on the offer box.
-  test('should open second search page and open any hotel info box', async ({ homePage, searchResultPage }) => {
-    await homePage.destinationFields().click()
-    await homePage.destinationFields().fill('Egy')
-    await homePage.autoconmpleteItem(0).click()
-    await homePage.datesField().click()
-    await homePage.setDate('2025-07-02', '2025-07-12')
-    await homePage.clickSearchButton()
-    const offerInfo = await searchResultPage.getOfferInformation(2)
-    
-    const offerTab = await searchResultPage.goToOffer(2)
-    const offerPage = new OfferPage(offerTab)
-    const counfirmedOffers = await offerPage.countConfirmedOffers()
-
-    // Confirm first unconfimed offer
-    const testOfferNumber = counfirmedOffers + 1
-    expect(await offerPage.getOfferStatus(testOfferNumber)).toEqual('neutral')
-    const offer = await offerPage.clickConfirmOfferButton(testOfferNumber)
-    expect(await offerPage.getOfferStatus(testOfferNumber)).toEqual('success')
-  })
+      // Confirm first unconfimed offer
+      const testOfferNumber = counfirmedOffers + 1
+      expect(await offerPage.getOfferStatus(testOfferNumber)).toEqual('neutral')
+      const offer = await offerPage.clickConfirmOfferButton(testOfferNumber)
+      expect(await offerPage.getOfferStatus(testOfferNumber)).toEqual('success')
+    })
+  }
 })
